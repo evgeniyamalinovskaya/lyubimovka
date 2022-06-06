f17ce8627c37ae36d285234e18243b6c0615d27c
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-06-06 11:31:06 +0300

    add video-preview, JS script

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const videoIframe = document.querySelector('.video__iframe');
+const playButton = document.querySelector('.video__play-btn');
+function playVideo(evt) {
+videoIframe.classList.add('video__iframe_active')
+};
+playButton.addEventListener('click', playVideo);
