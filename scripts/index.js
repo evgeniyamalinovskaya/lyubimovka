829087a21ce770999131f5f6a9cc3ffd22e2a40a
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-06-06 17:37:13 +0300

    in process to add adaptive

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 const videoIframe = document.querySelector('.video__iframe');
 const playButton = document.querySelector('.video__play-btn');
+
+//Активация видео
 function playVideo(evt) {
-videoIframe.classList.add('video__iframe_active')
+    videoIframe.classList.add('video__iframe_active')
 };
+
 playButton.addEventListener('click', playVideo);
