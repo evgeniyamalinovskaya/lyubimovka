5fe37dd89d77ea8fd4a1558e8b686821f0a5795b
Sonja Kuvshinova <kuvshinova.sonja@yandex.ru>
2022-06-07 12:22:06 +0300

    desktop version is ready

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
 const videoIframe = document.querySelector('.video__iframe');
+const previewImage = document.querySelector('.video__preview');
 const playButton = document.querySelector('.video__play-btn');
 
-//Активация видео
+
+//Активация видео и скрытие превью-картинки
 function playVideo(evt) {
-    videoIframe.classList.add('video__iframe_active')
+    videoIframe.classList.add('video__iframe_active');
+    previewImage.classList.add('video__preview_disactive');
 };
 
 playButton.addEventListener('click', playVideo);
